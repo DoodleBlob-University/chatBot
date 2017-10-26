2327777d46025c45bf9a5fe18492a5f563e0636f
Dominic Egginton <dominic.egginton@gmail.com>
2017-10-26 09:34:16 +0100

    added pass args func and header func

--- client/client.py
+++ client/client.py
@@ -5,9 +5,22 @@ import socket
 import threading
 import argparse
 
+def getArgs():
+    ''' getArgs returns all program arguments '''
+    parser = argparse.ArgumentParser(description='') # Add description
+    parser.add_argument('-p', '--port', metavar='Port', type=int, default=1143, help='Server port')
+    parser.add_argument('-a', '--address', metavar='Address', required=True, type=str, help='Server address')
+    return parser.parse_args()
+
+def drawHeader():
+    ''' draws program ui header '''
+    print('*** Client Header ***\nWelcome\n\n')
+
 def main():
     ''' main '''
-    pass
+    args = getArgs()
+    drawHeader()
+    print(args.port)
 
 if __name__ == '__main__':
     main()
