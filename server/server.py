d76cc6916b88719f1a8077060615bee66a717cec
Dominic Egginton <dominic.egginton@gmail.com>
2017-10-26 02:25:36 +0100

    added basic networking code

--- server/server.py
+++ server/server.py
@@ -3,13 +3,56 @@
 
 import socket
 import threading
-import sys
+import argparse
+
+class server(object):
+    def __init__(self, hostIP, hostPort):
+        self.hostIP = hostIP
+        self.hostPort = hostPort
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.hostIP, self.hostPort))
+
+    def serverListen(self):
+        self.socket.listen(15)
+        while True:
+            try:
+                client, clientAddress = self.socket.accept()
+                client.settimeout(120)
+                threading.Thread(target=self.receiveFromClient,args = (client, clientAddress)).start()
+                print('** Client Connected {}'.format(clientAddress))
+            except:
+                raise Exception('Client connection error')
+        
+    def receiveFromClient(self, client, clientAddress):
+        byteSize = 1024
+        while True:
+            try:
+                receivedData = client.recv(byteSize)
+                if receivedData and type(receivedData) == bytes:
+                    # receivedStr = receivedData.decode()
+                    # send to language decoder
+                    pass
+                else:
+                    print('** Client Disconeted {}'.format(clientAddress))
+                    client.close()
+                    return False
+            except:
+                client.close()
+                return False
+
+def getArgs():
+    ''' getArgs returns all program arguments '''
+    parser = argparse.ArgumentParser(description='') # Add description 
+    parser.add_argument('-p', '--port', metavar='Port', type=int, help='Server port')
+    return parser.parse_args()
 
 def main():
-    pass
-
-
-
+    ''' main '''
+    # Add header and UI
+    args = getArgs()
+    if args.port:
+        server('', args.port).serverListen()
 
 if __name__ == '__main__':
     main()
