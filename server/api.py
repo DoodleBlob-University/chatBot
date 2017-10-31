6d1ff5aade7b619b40ea2b63ef0b0ed6dba893dd
Mitko Donchev (donchevm) <donchevm@coventry.ac.uk>
2017-10-31 15:44:55 +0000

    Update api.py

--- server/api.py
+++ server/api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import requests
+import urllib.request
+import json
 
 def apiCall(apiURL):
     request = requests.get(apiURL)
@@ -7,3 +9,19 @@ def apiCall(apiURL):
 
 if __name__ == '__main__':
     pass ## add tests here
+
+
+
+
+#http://api.cinelist.co.uk/search/cinemas/postcode/LU12HN 
+def locu_search(post_code):    
+    url = 'http://api.cinelist.co.uk/search/cinemas/postcode/' 
+    locality = post_code.replace(' ', '%20')
+    final_url = url + locality 
+    response = urllib.request.urlopen(final_url).read() 
+    json_obj = str(response, 'utf-8')
+    data = json.loads(json_obj)   
+    for item in data['cinemas']:
+        print (item['name'], item['distance'])
+locu_search("CV12HR") 
+#finding nearest cinema using postcode
