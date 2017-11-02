91a9ed6456f02698cd0fac6410df97182cff5f33
Charlie Barry <barryc2@coventry.ac.uk>
2017-11-02 14:15:40 +0000

    made client ui header look pretty

--- client/client.py
+++ client/client.py
@@ -4,6 +4,9 @@
 import socket
 import threading
 import argparse
+import os
+import subprocess
+from colorama import Fore, Style
 
 def getArgs():
     ''' getArgs returns all program arguments '''
@@ -12,9 +15,20 @@ def getArgs():
     parser.add_argument('-a', '--address', metavar='Address', required=True, type=str, help='Server address')
     return parser.parse_args()
 
+def clear():
+    ''' clears bash terminal display '''
+    if os.name in ('nt','dos'):
+        subprocess.call("cls")
+    elif os.name in ('linux','osx','posix'):
+        subprocess.call("clear")
+    else:
+        print("\n"*120)
+
 def drawHeader():
     ''' draws program ui header '''
-    print('*** Client Header ***\nWelcome\n\n')
+    clear()
+    print(Fore.GREEN + " _____ _           _    ______       _   \n" + Fore.BLUE + "/  __ \ |         | |   | ___ \     | |  \n" + Fore.MAGENTA + "| /  \/ |__   __ _| |_  | |_/ / ___ | |_ \n" + Fore.RED + "| |   | '_ \ / _` | __| | ___ \/ _ \| __|\n" + Fore.YELLOW + "| \__/\ | | | (_| | |_  | |_/ / (_) | |_ \n" + Fore.CYAN + " \____/_| |_|\__,_|\__| \____/ \___/ \__|\n\n" + Style.RESET_ALL + " "*17 + 'Welcome!' )
+
 
 def main():
     ''' main '''
