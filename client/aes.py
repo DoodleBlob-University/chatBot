e33f9e2869dbeb47f9b3383a888ab8e84a68f2d2
Charlie Barry <barryc2@coventry.ac.uk>
2017-11-20 16:21:27 +0000

    AES encryption works for strings larger than 16 bytes

--- client/aes.py
+++ client/aes.py
@@ -5,20 +5,20 @@ from Crypto.Cipher import AES
 
 class AESEncryption(object):
 
-    def __init__(self, key): 
+    def __init__(self, key):
         self.bs = 32
         self.key = hashlib.sha256(key.encode()).digest()
 
     def encrypt(self, rawData):
         rawData = self._pad(rawData)
         iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        cipher = AES.new(self.key, AES.MODE_CFB, iv)
         return base64.b64encode(iv + cipher.encrypt(rawData))
 
     def decrypt(self, encrypted):
         encrypted = base64.b64decode(encrypted)
         iv = encrypted[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        cipher = AES.new(self.key, AES.MODE_CFB, iv)
         return self._unpad(cipher.decrypt(encrypted[AES.block_size:])).decode('utf-8')
 
     def _pad(self, s):
