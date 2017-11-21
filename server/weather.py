578ab046517b9469cdb3bec42f5e7b673a834844
Charlie Barry <barryc2@coventry.ac.uk>
2017-11-21 02:21:19 +0000

    Moved code for weather into class within weather.py - also shortened it :)

--- server/weather.py
+++ server/weather.py
@@ -2,15 +2,41 @@
 import requests
 
 class weather(object):
-    def __init__(self, coordinates):
-        self.coordinates = coordinates
+    def __init__(self,):
         self.forcastApiKey = '1cd16597539dafae0c09187ef4dc19bc'
-        self.url = 'https://api.darksky.net/forecast/{}/{},{}?units=si'.format(self.forcastApiKey, self.coordinates['latitude'], self.coordinates['longitude'])
-        self.weatherData = self.forcastRequest(self.url)
-        self.currently = self.weatherData['currently']
-        self.daily = self.weatherData['daily']
-        self.hourly = self.weatherData['hourly']
 
-    def forcastRequest(self, url):
+    def forcastRequest(self, location):
+        url = 'https://api.darksky.net/forecast/{}/{},{}?units=si'.format(self.forcastApiKey, location.get('latitude'), location.get('longitude'))
         request = requests.get(url)
         return request.json()
+
+    def weatherResponse(self, keysFound, clientaddress, extra):
+        location = {}
+        locationStr = ""
+        if 'location' in keysFound:
+            from geocode import geocode
+            geoCode = geocode()
+            location = geoCode.getLocationCoords(extra.get('location'))
+            locationStr = " in {}".format(extra.get('location').capitalize())
+        else:
+            location = {'latitude': clientaddress['lat'], 'longitude': clientaddress['lon']}
+        weatherData = self.forcastRequest(location)
+
+        if 'time' not in keysFound:
+            return 'It is currently {}{} and the temperature is {} Celsius'.format(weatherData['currently']['summary'],locationStr,str(weatherData['currently']['temperature']))
+
+        if extra.get('time') == 'daily':
+            response = 'Daily Weather Forcast{}:\nSummary: {}\n\n'.format(locationStr, str(weatherData['daily']['summary']))
+            for day in weatherData['daily']['data']:
+                response = response + '{}:\nSummary: {}\nMax: {} @ {}\nMin: {} @ {}\n\n'.format(self.unixTimeToDateTime(day['time']),day['summary'],day['temperatureMax'],self.unixTimeToDateTime(day['temperatureMaxTime']),day['temperatureMin'],self.unixTimeToDateTime(day['temperatureMinTime']))
+            return response
+        elif extra.get('time') == 'hourly':
+            response = 'Hourly Weather Forcast{}:\nSummary: {}\n\n'.format(locationStr, str(weatherData['hourly']['summary']))
+            for day in weatherData['hourly']['data']:
+                response = response + '{}:\nSummary: {}\nTempature: {} Celsius\n\n'.format(self.unixTimeToDateTime(day['time']),day['summary'],day['temperature'])
+            return response
+        return None
+
+    def unixTimeToDateTime(self, unixTime):
+        import datetime
+        return datetime.datetime.fromtimestamp(int("1284101485")).strftime('%Y-%m-%d %H:%M')
