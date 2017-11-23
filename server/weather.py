b35becbc186b5153ffe5590c65d101aacc26604f
Dominic Egginton <eggintod@coventry.ac.uk>
2017-11-23 13:20:16 +0000

    added comments and docstrings

--- server/weather.py
+++ server/weather.py
@@ -1,16 +1,18 @@
-#!/usr/bin/python
-import requests
+import requests ### imported module requests <https://pypi.python.org/pypi/requests>
 
-class weather(object):
-    def __init__(self):
+class weather(object): ### Dominic Egginton
+    ''' weather class takes no arguments to initialise '''
+    def __init__(self): ### Dominic Egginton
         self.forcastApiKey = '1cd16597539dafae0c09187ef4dc19bc'
 
-    def forcastRequest(self, location):
+    def forcastRequest(self, location): ### Denola
+        ''' forcastRequest takes location as a dictionary that contains the lat and lon values and returns weather data as josn object '''
         url = 'https://api.darksky.net/forecast/{}/{},{}?units=si'.format(self.forcastApiKey, location.get('latitude'), location.get('longitude'))
         request = requests.get(url)
         return request.json()
 
-    def weatherResponse(self, keysFound, clientaddress, extra):
+    def weatherResponse(self, keysFound, clientaddress, extra): ### Dominic Egginton
+        ''' weatherResponse takes keysFound in client test, the client adrress and extra data and retunes a formatted string '''
         location = {}
         locationStr = ""
         if 'location' in keysFound:
@@ -45,6 +47,7 @@ class weather(object):
             return response
         return None
 
-    def unixTimeToDateTime(self, unixTime):
+    def unixTimeToDateTime(self, unixTime): ### Dominic Egginton
+        ''' unixTimeToDateTime takes a string of unix time and returns a formatted date and time as a string '''
         import datetime
         return datetime.datetime.fromtimestamp(int(unixTime)).strftime('%Y-%m-%d %H:%M')
