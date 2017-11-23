e58b154c2079b5a6c523548771085e30f3e79c48
Dominic Egginton <eggintod@coventry.ac.uk>
2017-11-23 18:51:32 +0000

    added some more tests

--- server/tests.py
+++ server/tests.py
@@ -6,24 +6,46 @@ class testServer(unittest.TestCase):
         from server import server
         self.server = server('', 1143, 'gbaei395y27ny9')
 
-    def test_getIpData(self):
+    def test_getIpData(self): ### Dominc Egginton
         self.assertEqual(self.server.getIpData('8.8.8.8')['status'], 'success')
         self.assertEqual(self.server.getIpData('')['status'], 'success')
         self.assertEqual(self.server.getIpData('127.0.0.1')['status'], 'success')
 
+    def test_formResponse(self): ### Dominic Egginton
+        self.assertNotEqual(self.server.formResponse('Hey, what is the weather', '127.0.0.1'), "Sorry, I don't understand what you are talking about.")
+        self.assertNotEqual(self.server.formResponse('Whats the weather like in london?', '127.0.0.1'), "Sorry, I don't understand what you are talking about.")
+        self.assertNotEqual(self.server.formResponse('Can you get me the daily weather in Hinckley', '127.0.0.1'), "Sorry, I don't understand what you are talking about.")
+        self.assertNotEqual(self.server.formResponse('grab me the hourly weather', '127.0.0.1'), "Sorry, I don't understand what you are talking about.")
+        self.assertNotEqual(self.server.formResponse('1 gbp to usd', '127.0.0.1'), "Sorry, I don't understand what you are talking about.")
+        self.assertEqual(self.server.formResponse('open celery', '127.0.0.1'), "/w/https://youtu.be/MHWBEK8w_YY")
+        self.assertEqual(self.server.formResponse('get me the fucking weather', '127.0.0.1'), "Please watch your language.")
+        self.assertEqual(self.server.formResponse('am i talking about cinema ?', '127.0.0.1'), "You are talking about cinema")
+        self.assertEqual(self.server.formResponse('Hey you whats up ?', '127.0.0.1'), "Sorry, I don't understand what you are talking about.")
+        self.assertEqual(self.server.formResponse('Whats my ip info ?', '8.8.8.8'), "Your IP is 8.8.8.8, provided by Google.")
+
     def test_searchJSON(self): ### Charlie Barry
         self.assertEqual(self.server.searchJSON('This has no keywords'), ([], {}))              #checks if response is correct when no keywords are present
         self.assertEqual(self.server.searchJSON('Hi what is the weather'), (['weather'], {}))   #tests whether it can recognise a keyword
-        self.assertEqual(self.server.searchJSON('Is it going to rain in Coventry'), (['location', 'weather'], {'location': 'Coventry'}) or (['weather', 'location'], {'location': 'Coventry'})) #tests whether it can recognise a keyword and fetch location name
+        self.assertEqual(self.server.searchJSON('Is it going to rain in Coventry'), (['weather', 'location'], {'location': 'Coventry'}) or (['weather', 'location'], {'location': 'Coventry'})) #tests whether it can recognise a keyword and fetch location name
 
-class weather(unittest.TestCase):
+    def closeSocket(self):
+        self.server.socket.close()
+
+class testWeather(unittest.TestCase): ### Dominic Egginton
+
     def setUp(self):
         from weather import weather
-        self.weather = weather
+        self.weather = weather()
 
     def test_unixTimeToDateTime(self):
-        self.assertEqual(self.weather.unixTimeToDateTime(self, '1511276400'), '2017-11-21 15:00')
-        self.assertEqual(self.weather.unixTimeToDateTime(self, '910094199'), '1998-11-03 11:56')
+        self.assertEqual(self.weather.unixTimeToDateTime('1511276400'), '2017-11-21 15:00')
+        self.assertEqual(self.weather.unixTimeToDateTime('910094199'), '1998-11-03 11:56')
+        self.assertEqual(self.weather.unixTimeToDateTime('1511454788'), '2017-11-23 16:33')
+
+    def test_weatherResponse(self):
+        self.assertTrue('currently' in self.weather.forcastRequest({'latitude': '37.8267', 'longitude': '-122.4233'}))
+        self.assertTrue('daily' in self.weather.forcastRequest({'latitude': '37.8267', 'longitude': '-122.4233'}))
+        self.assertTrue('hourly' in self.weather.forcastRequest({'latitude': '37.8267', 'longitude': '-122.4233'}))
 
 class TestAES(unittest.TestCase): ### Charlie Barry
     '''tests aes to ensure that encrypted strings can be decrypted'''
