147ea59e7fce4e21427bbf86949d9c7c1ddb34e7
Charlie Barry <barryc2@coventry.ac.uk>
2017-11-23 13:31:01 +0000

    Deleted useless files and added comments

--- server/aes.py
+++ server/aes.py
@@ -9,17 +9,17 @@ class AESEncryption(object):
         self.bs = 32
         self.key = hashlib.sha256(key.encode()).digest()
 
-    def encrypt(self, rawData):
-        rawData = self._pad(rawData)
+    def encrypt(self, plaintext):
+        plaintext = self._pad(plaintext)    #Pads plaintext so its size is a multiple of 16 bytes
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CFB, iv)
-        return base64.b64encode(iv + cipher.encrypt(rawData))
+        return base64.b64encode(iv + cipher.encrypt(plaintext))
 
-    def decrypt(self, encrypted):
-        encrypted = base64.b64decode(encrypted)
-        iv = encrypted[:AES.block_size]
+    def decrypt(self, encryptedStr):
+        encryptedStr = base64.b64decode(encryptedStr)
+        iv = encryptedStr[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CFB, iv)
-        return self._unpad(cipher.decrypt(encrypted[AES.block_size:])).decode('utf-8')
+        return self._unpad(cipher.decrypt(encryptedStr[AES.block_size:])).decode('utf-8')
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
