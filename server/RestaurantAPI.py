0eeadbe8c4e696176bc8f8856f94a3eb35e31a41
Matthew Cheung (cheun227) <cheun227@coventry.ac.uk>
2017-11-23 14:01:33 +0000

    Update RestaurantAPI.py

--- server/RestaurantAPI.py
+++ server/RestaurantAPI.py
@@ -1,14 +1,14 @@
 ##### Function to search google places ratings!!!!!!! #####
 def getGooglePlaces(restaurants, location, keyword):
-    # 'restaurants': array of dictionaries = [{u"name":u"Jimbo's",u"location":u"Coventry"},{u"name":u"Taste of China",u"location":u"London"}]
- 
+    # 'restaurants': array of dictionaries = [{u"name":u"Jimbo's",u"location":u"Coventry"}
     # Set google places api
     from googleplaces import GooglePlaces, types, lang
     google_places_api_key = 'AIzaSyDQ-mcgK1gSnI6soXWZnAA2Z9MeDnb5ZRo'
     google_places = GooglePlaces(google_places_api_key)
       
-     query_result = google_places.nearby_search(location, keyword,radius=1000, types=[types.TYPE_RESTAURANT])
+    query_result = google_places.nearby_search(location, keyword,radius=1000, types=[types.TYPE_RESTAURANT])
 
+    try:
     if query_result.has_attributions:
         queryResult = (query_result.html_attributions)
 
@@ -17,7 +17,7 @@ def getGooglePlaces(restaurants, location, keyword):
         geoLocation = (place.geo_location)
         placeId = (place.place_id)
          
-        # Loop query results
+        # Check and grab restaurant details
         for place in query_result.places:
             if (restaurantName == place.name.lower() or
                 'restaurant ' + restaurantName  == place.name.lower() or
@@ -32,5 +32,8 @@ def getGooglePlaces(restaurants, location, keyword):
                 result = (place.name, '\t', place.rating, '\t', restarurantNumRatings) #\t will tab the results 
                 restaurantFound = 1
                 break
-
+                
+    except:
+        result = ("Not a valid restaurant")
+                    
     return(result, queryResult, name, geoLocation, placeId)
